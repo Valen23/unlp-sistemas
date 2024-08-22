c5995d8741a71d1982e4b8bd63fb54378b5437c3
Valentino <43416595+Valen23@users.noreply.github.com>
2024-08-21 21:46:18 -0300

    Create actividad3a.pas

---
+++ Taller de programación/practica 0/actividad3a.pas
@@ -0,0 +1,46 @@
+program ejercicio;
+
+type
+lista = ^nodo;
+
+nodo = record
+	dato: integer;
+	nodoSig: ^nodo;
+end;
+
+procedure agregarAdelante(var l:lista; num:integer);
+var
+	aux: lista;
+begin
+	new(aux); aux^.dato:=num; aux^.nodoSig:=l;
+	l:=aux;
+end;
+
+procedure CargarLista(var l:lista);
+var
+	num:integer;
+begin
+	num:=random(51)+100;
+	while (num <> 120) do
+		begin
+			agregarAdelante(l, num);
+			num:=random(51)+100;
+		end;
+end;
+
+procedure ImprimirLista(l:lista);
+begin
+	while (l <> nil) do
+		begin
+			writeln(l^.dato);
+			l:=l^.nodoSig;
+		end;
+end;
+
+var
+	l:lista;
+begin
+	randomize;
+	CargarLista(l);
+	ImprimirLista(l);
+end.
