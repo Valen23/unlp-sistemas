b4db831f7d0bebb380b23836426c0e6dd1f7d5f0
Valentino <43416595+Valen23@users.noreply.github.com>
2024-08-29 11:46:14 -0300

    Create practica2ej3.pas

---
+++ Taller de programación/practica 2/practica2ej3.pas
@@ -0,0 +1,16 @@
+program practica2ej4;
+procedure binarioRecursivo(num:integer);
+begin
+	if (num > 0) then
+		begin
+			binarioRecursivo(num div 2);
+			write(num mod 2);
+		end;
+end;
+	
+var
+  num:integer;
+begin
+	write('Escriba un numero: '); readln(num);
+	binarioRecursivo(num);
+end.
