2f850936f9d01a4755c753c3c6736754a9f8ccc1
Valentino <valengentile23@hotmail.com>
2024-09-16 19:23:50 -0300

    subcarpetas

---
+++ Taller de programación/pascal (modulo imperativo)/practica 0/actividad4.pas
@@ -0,0 +1,67 @@
+program actividad4;
+type
+	lista = ^nodo;
+	nodo = record
+		dato:integer;
+		nodoSig:lista;
+	end;
+
+procedure insertarOrdenado(var l:lista; num:integer);
+var
+	nue, act, ant:lista;
+begin
+	new(nue); nue^.dato:=num; act:=l;
+	ant:=l;
+	
+	while(act<>nil)and(num>act^.dato)do begin
+		ant:=act;
+		act:=act^.nodoSig;
+	end;
+	
+	if(act=ant)then
+		l:=nue
+	else
+		ant^.nodoSig:=nue;
+	nue^.nodoSig:=act;
+end;
+
+procedure cargarLista(var l:lista);
+var
+	num:integer;
+begin
+	num:=random(50)+100;
+	while (num <> 120) do
+		begin
+			insertarOrdenado(l, num);
+			num:=random(50)+100;
+		end;
+end;
+
+procedure imprimirLista(l:lista);
+begin
+	while (l <> nil) do
+		begin
+			writeln(l^.dato);
+			l:=l^.nodoSig;
+		end;
+end;
+
+function buscarElemento(l:lista; n:integer):boolean;
+begin
+	buscarElemento:=false;
+	while(l <> nil) do begin
+		if(l^.dato=n)then begin
+			buscarElemento:=true;
+		end;
+		l:=l^.nodoSig;
+	end;
+end;
+
+var
+	l:lista;
+begin
+	randomize;
+	cargarLista(l);
+	imprimirLista(l);
+	writeln(buscarElemento(l, 140));
+end.
