05a79a6c879f6110acc11b470b015a6d06b37ee0
Valentino <43416595+Valen23@users.noreply.github.com>
2024-09-25 19:11:40 -0300

    update ej4tema2

--- Taller de programación/java (modulo objetos)/facultad/src/tema2/ejercicio4.java
+++ Taller de programación/java (modulo objetos)/facultad/src/tema2/ejercicio4.java
@@ -14,33 +14,32 @@ public class ejercicio4 {
         int j = 0;
         
         Persona[][] matriz = new Persona[cantidadDias][cantidadPersonas];
-        String nombreAux = " ";
+        String nombreAux;
         int contadorAux = 0;
         int dia;
         int horario;
+
+        System.out.print("Ingrese su nombre: "); // Primero leo el nombre para verificar si crear un nuevo espacio
+        nombreAux = Lector.leerString();
         
         while(!nombreAux.equals("Z")&&(i < cantidadDias)&&(contadorAux < 40)){ // Verificamos que el nombre sea distinto de "Z" y que no desborde el indice 'i'
             
             System.out.print("Ingrese un dia: "); // Leo el dia y el horario
             dia = Lector.leerInt();
-            System.out.print("Ingrese un horario: ");
-            horario = Lector.leerInt();
             
-            System.out.print("Ingrese su nombre: "); // Primero leo el nombre para verificar si crear un nuevo espacio
-            nombreAux = Lector.leerString();
-            
-            if((matriz[dia][horario] == null)&&(!nombreAux.equals("Z"))){ // Si la posicion no está ocupada y el nombre ingresado no es "Z" se crea un nuevo espacio
+            if((vector[dia] < 8)&&(!nombreAux.equals("Z"))){ // Si la posicion no estÃ¡ ocupada y el nombre ingresado no es "Z" se crea un nuevo espacio
                 contadorAux++; // Aumento el contador
                 
-                matriz[dia][horario] = new Persona(); // Nuevo espacio
+                matriz[dia][vector[dia]] = new Persona(); // Nuevo espacio
                 
-                matriz[dia][horario].setNombre(nombreAux);
+                matriz[dia][vector[dia]].setNombre(nombreAux);
                 System.out.print("Ingrese su DNI: ");
-                matriz[dia][horario].setDNI(Lector.leerInt());
+                matriz[dia][vector[dia]].setDNI(Lector.leerInt());
                 System.out.print("Ingrese su edad: ");
-                matriz[dia][horario].setEdad(Lector.leerInt());
-            } else if(matriz[dia][horario] != null){
-                System.out.println("Posicion invalida, el turno de ese dia esta ocupado!");
+                matriz[dia][vector[dia]].setEdad(Lector.leerInt());
+                vector[dia]++;
+            } else {
+                System.out.println("Dia invalido, el dia esta completo!");
             }
         }
         
