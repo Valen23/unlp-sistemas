b2d22c710f90443b1eafbe5ba453c078b32162de
Valentino <valengentile23@hotmail.com>
2024-10-28 20:26:10 -0300

    upd

--- Arquitectura de Computadoras/winmips/parte2-5c.s
+++ Arquitectura de Computadoras/winmips/parte2-5c.s
@@ -1,7 +1,7 @@
 .data
 X: .byte 0
 Y: .byte 0
-color: .byte 255, 0, 0, 0
+color: .byte 255, 255, 0, 0
 CONTROL: .word32 0x10000
 DATA: .word32 0x10008
 
@@ -17,11 +17,11 @@ lbu $t2, X($zero) # carga en $t2 la coordenada X
 daddi $t4, $zero, 50 # se usa para comparar el fin
 daddi $t5, $zero, 50 # se usa para comparar el fin
 loop:
-sw $t0, 0($s1) # manda a DATA $t0, (COLOR)
+sb $t0, 1($s1) # manda a DATA $t0, (COLOR)
 sb $t1, 4($s1) # almaceno lo que hay en $t1 en DATA(Y)
 sb $t2, 5($s1) # almaceno lo que hay en $t2 en DATA(X) __________________
 daddi $t3, $zero, 5 # funcion 5
-sd $t3,0($s0) # mando a control 5
+sd $t3, 0($s0) # mando a control 5
 daddi $t2, $t2, 1 # a la coordenada X le suma 1
 bne $t4, $t2, loop # si no $t2 != 50 salta
 daddi $t2, $zero, 0 # hago que vuelva a la coordenada X = 0 __________________
